4ee448d83a3d123f5c51b4ec51835871a514f622
aris1377 <bek77519101@gmail.com>
2024-06-05 13:00:39 +0900

    feat: MIT 14 TASK G

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import moment from "moment";
-const currentTime = moment().format('YYYY MM DD')
-console.log(currentTime);
+// biz har doim architector va design pettenrlarga murojat qilamiz
+//bu nima uchun kk? biz bir tildan boshqa tilga otganda oson ozlashtirishimiz uchun
 
-const person: string = "Martin";
-const count: number = 100;
+//architectural bu - bakcend suyagi yani malumotlar oqimini tartibga soladigan bir vosita
+//eng mashxur architectural pettern bu - backend -> (MVS ( Model-View-Controller (MVC) pattern))
+//fronted -> (MVP - murakkab foydalanuvchi interfeyslari bo'lgan ilovalar uchun yanada sinovdan o'tkaziladigan va qo'llab-quvvatlanadigan kod tuzilishini ta'minlash uchun mo'ljallangan)
+
+//design bu - backendni malum bir bolaklarini boshqarishda xizmat qiladigan pettern
+//middleware, Decotar lar kiradi
---
+++ train.ts
@@ -0,0 +1,30 @@
+// MIT 14 TASK G:
+
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning taribidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+const getHighestIndex = (numbers: number[]): number => {
+  if (numbers.length === 0) {
+    throw new Error("Error");
+  }
+
+  let maxIndex = 0;
+  let maxValue = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxValue) {
+      maxValue = numbers[i];
+      maxIndex = i;
+    }
+  }
+
+  return maxIndex;
+};
+
+const numbers = [5, 21, 12, 21, 8];
+console.log(getHighestIndex(numbers));
