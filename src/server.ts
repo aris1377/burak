12059b7ff3c1133e81ac44722afb8a9c0ac9a286
aris1377 <bek77519101@gmail.com>
2024-06-06 09:37:14 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-// biz har doim architector va design pettenrlarga murojat qilamiz
-//bu nima uchun kk? biz bir tildan boshqa tilga otganda oson ozlashtirishimiz uchun
+// dotenv package ichidan <dotenv> ni cha1irib berishni sorayapmiz
+import dotenv from "dotenv";
 
-//architectural bu - bakcend suyagi yani malumotlar oqimini tartibga soladigan bir vosita
-//eng mashxur architectural pettern bu - backend -> (MVS ( Model-View-Controller (MVC) pattern))
-//fronted -> (MVP - murakkab foydalanuvchi interfeyslari bo'lgan ilovalar uchun yanada sinovdan o'tkaziladigan va qo'llab-quvvatlanadigan kod tuzilishini ta'minlash uchun mo'ljallangan)
+//documentionga qarasak <dotenv> ni <config> degan methodi mavjud va usha <config> methodini excuit qilyapmiz
+//biz dotenv varibillarni intragatsiyalini amalga oshirdik
+dotenv.config();
 
-//design bu - backendni malum bir bolaklarini boshqarishda xizmat qiladigan pettern
-//middleware, Decotar lar kiradi
+//togri ishlayotganini tekshrish uchun
+//PROSSES ENV ichidan PORT ni charib olamiz
+// console.log("PORT:", process.env.PORT);
+//yani bizni .ENV  variabllar ichidan DOTENV oqishga imkon hosil qildi
+
+//MONGO_URL ni test qilib olamiz
+// console.log("MONGO_URL:", process.env.MONGO_URL);
--- train.ts
+++ train.ts
@@ -1,3 +1,19 @@
+// shunday function tuzing, u integerlardan iborat, arrayni argument sifatida qabul qilib,
+// faqat positive qiymatlarni olib string holatda return qilsin
+// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+// function getPositive(numbers: number[]): string {
+//   const positiveStrings = numbers
+//     .filter((num) => num > 0)
+//     .map((num) => num.toString());
+
+//   return positiveStrings.join("");
+// }
+
+// // Example usage
+// const result = getPositive([1, -4, 2]);
+// console.log(result); // Output: "12"
+
 // MIT 14 TASK G:
 
 // Yagona parametrga ega function tuzing.
@@ -8,23 +24,23 @@
 // Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
 // Va bu 21 soni arrayning taribidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
 
-const getHighestIndex = (numbers: number[]): number => {
-  if (numbers.length === 0) {
-    throw new Error("Error");
-  }
+// const getHighestIndex = (numbers: number[]): number => {
+//   if (numbers.length === 0) {
+//     throw new Error("Error");
+//   }
 
-  let maxIndex = 0;
-  let maxValue = numbers[0];
+//   let maxIndex = 0;
+//   let maxValue = numbers[0];
 
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > maxValue) {
-      maxValue = numbers[i];
-      maxIndex = i;
-    }
-  }
+//   for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > maxValue) {
+//       maxValue = numbers[i];
+//       maxIndex = i;
+//     }
+//   }
 
-  return maxIndex;
-};
+//   return maxIndex;
+// };
 
-const numbers = [5, 21, 12, 21, 8];
-console.log(getHighestIndex(numbers));
+// const numbers = [5, 21, 12, 21, 8];
+// console.log(getHighestIndex(numbers));
