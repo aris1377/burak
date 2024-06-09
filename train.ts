ea028701d6c3957c5ffdbad6cc5c6287f9b21102
aris1377 <bek77519101@gmail.com>
2024-06-09 09:18:36 +0900

    feat: Lesson TASK H2

--- train.ts
+++ train.ts
@@ -1,18 +1,39 @@
-// H-TASK: 
+// 39th Lesson TASK H2
+
+// String argument pass bo'ladigan function tuzing.
+// Ushbu function argument tarkibidagi digit(son)larni topib
+// yangi stringda return qilsin
+
+// MASALAN: getDigits("m14i1t"); return qiladi "141"
+// Yuqoridagi misolda, berilayotgan parametr tarkibida ham harf ham son mavjud.
+// Natija sifatida text tarkibidagi sonlarni aniqlab ularni string ko'rinishida qaytarmoqdamiz
+function getDigits(inputString: string): string {
+  let digits = "";
+  for (let i = 0; i < inputString.length; i++) {
+    if (inputString[i] >= "0" && inputString[i] <= "9") {
+      digits += inputString[i];
+    }
+  }
+  return digits;
+}
+let result = getDigits("m14i1t");
+console.log(result);
+
+// H-TASK:
 // shunday function tuzing, u integerlardan iborat, arrayni argument sifatida qabul qilib,
 // faqat positive qiymatlarni olib string holatda return qilsin
 // MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
-function getPositive(numbers: number[]): string {
-  const positiveStrings = numbers
-    .filter((num) => num > 0)
-    .map((num) => num.toString());
+// function getPositive(numbers: number[]): string {
+//   const positiveStrings = numbers
+//     .filter((num) => num > 0)
+//     .map((num) => num.toString());
 
-  return positiveStrings.join("");
-}
+//   return positiveStrings.join("");
+// }
 
-const result = getPositive([1, -4, 2]);
-console.log(result);
+// const result = getPositive([1, -4, 2]);
+// console.log(result);
 
 // MIT 14 TASK G:
 
