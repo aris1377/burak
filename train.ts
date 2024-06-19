79e80e819ff411b445698aa241e3bca1cb472deb
aris1377 <bek77519101@gmail.com>
2024-06-19 17:00:30 +0900

    feat: TASK J

--- train.ts
+++ train.ts
@@ -1,3 +1,29 @@
+// 2024-06-18
+// MIT 14
+// TASK J:
+
+// Shunday function tuzing, u string qabul qilsin.
+// Va string ichidagi eng uzun so'zni qaytarsin.
+
+// MASALAN: findLongestWord("I came from Uzbekistan!"); return "Uzbekistan!"
+
+// Yuqoridagi text tarkibida 'Uzbekistan'
+// eng uzun so'z bo'lganligi uchun 'Uzbekistan'ni qaytarmoqda
+
+function findLongestWord(str: string): string {
+  const words = str.split(" ");
+
+  let longestWord = "";
+
+  for (const word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+}
+console.log(findLongestWord("I came from Uzbekistan!"));
+
 // MIT 14
 // TASK I:
 
@@ -119,7 +145,7 @@
   3.GraphQl Api
  */
 
-  /**
+/**
  Traditional FD -> SSR -> EJS
  Modern FD      -> SPA -> REACT (LIBERY)
  */
