8f9b738ffde3562dbc5f6cf7ba571fb95068c466
aris1377 <bek77519101@gmail.com>
2024-06-29 19:08:47 +0900

    feat: O-TASK

--- train.ts
+++ train.ts
@@ -1,4 +1,21 @@
-// 2024-06-27
+//O-TASK:
+
+//Shunday function yozing, u har xil valuelardan iborat array qabul qilsin
+ //array ichidagi sonlar yigindisini hisoblab chiqqan javobni qaytarsin.
+//MASALAN: calculateSumOfNumbers([10, "10", {son: 10}, true, 35]) return 45
+
+function calculateSumOfNumbers(arr: any[]): number {
+    return arr.reduce((sum, item) => {
+        if (typeof item === 'number') {
+            return sum + item;
+        }
+        return sum;
+    }, 0);
+}
+console.log(calculateSumOfNumbers([10, "10", {son: 10}, true, 35])); 
+
+
+// 2024-06 - 27
 // MIT 14
 // TASK N:
 
