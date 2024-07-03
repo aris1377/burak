f04a2ab88caf37e167aff281d3a7e25c4a805899
aris1377 <bek77519101@gmail.com>
2024-07-03 13:36:30 +0900

    fix: admin project refactoring

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -6,9 +6,9 @@ $(function () {
 
   fileTarget.on("change", function () {
     if (window.FileReader) {
-      const uploadFile = $(this)[0].files[0];
-      const fileType = uploadFile["type"];
-      const validMimeTypes = ["image/jpg", "image/jpeg", "image/png"];
+      const uploadFile = $(this)[0].files[0],
+        fileType = uploadFile["type"],
+        validMimeTypes = ["image/jpg", "image/jpeg", "image/png"];
       if (!validMimeTypes.includes(fileType)) {
         alert("Please insert only jpg, jpeg, png");
       } else {
@@ -26,10 +26,10 @@ $(function () {
 });
 
 function validateSignupForm() {
-  const memberNick = $(".member-nick").val();
-  const memberPhone = $(".member-phone").val();
-  const memberPassword = $(".member-password").val();
-  const confirmPassword = $(".confirm-password").val();
+  const memberNick = $(".member-nick").val(),
+    memberPhone = $(".member-phone").val(),
+    memberPassword = $(".member-password").val(),
+    confirmPassword = $(".confirm-password").val();
 
   if (
     memberNick === "" ||
