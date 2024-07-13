db4845031afb5b099ad928757a51d83e688b97ac
aris1377 <bek77519101@gmail.com>
2024-07-14 05:17:50 +0900

    feat: MIT 14

--- train.ts
+++ train.ts
@@ -1,43 +1,67 @@
-// Shunday function yozing, 
-//u sonlardan tashkil topgan 2 ta array qabul qilsin 
+// 2024-07-13
+// MIT 14
+// TASK U
+
+// Shunday function tuzing, uni number parametri bo'lsin.
+// Va bu function berilgan parametrgacha, 0'dan boshlab
+// oraliqda nechta toq sonlar borligini aniqlab return qilsi.
+
+// MASALAN: sumOdds(9) return 4; sumOdds(11) return 5;
+
+// Yuqoridagi birinchi misolda, argument sifatida, 9 berilmoqda.
+// Va 0'dan boshlab sanaganda 9'gacha 4'ta toq son mavjud.
+// Keyingi namunada ham xuddi shunday xolat takrorlanmoqda.
+
+function sumOdds(num: number): number {
+  let count = -1;
+  for (let i = 0; i <= num; i++) {
+    if (i % 2 !== 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(sumOdds(9)); // 4
+console.log(sumOdds(11)); // 5
+
+// Shunday function yozing,
+//u sonlardan tashkil topgan 2 ta array qabul qilsin
 // va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
 // MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
 
+// function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+//   const mergedArray: number[] = [];
+//   let i = 0,
+//     j = 0;
 
-function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
-  const mergedArray: number[] = [];
-  let i = 0,
-    j = 0;
+//   while (i < arr1.length && j < arr2.length) {
+//     if (arr1[i] < arr2[j]) {
+//       mergedArray.push(arr1[i]);
+//       i++;
+//     } else {
+//       mergedArray.push(arr2[j]);
+//       j++;
+//     }
+//   }
 
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] < arr2[j]) {
-      mergedArray.push(arr1[i]);
-      i++;
-    } else {
-      mergedArray.push(arr2[j]);
-      j++;
-    }
-  }
+//   while (i < arr1.length) {
+//     mergedArray.push(arr1[i]);
+//     i++;
+//   }
 
- 
-  while (i < arr1.length) {
-    mergedArray.push(arr1[i]);
-    i++;
-  }
+//   while (j < arr2.length) {
+//     mergedArray.push(arr2[j]);
+//     j++;
+//   }
 
-  while (j < arr2.length) {
-    mergedArray.push(arr2[j]);
-    j++;
-  }
+//   return mergedArray;
+// }
 
-  return mergedArray;
-}
-
-
-const array1 = [0, 3, 4, 31];
-const array2 = [4, 6, 30];
-const mergedArray = mergeSortedArrays(array1, array2);
-console.log(mergedArray); 
+// const array1 = [0, 3, 4, 31];
+// const array2 = [4, 6, 30];
+// const mergedArray = mergeSortedArrays(array1, array2);
+// console.log(mergedArray);
 
 // 2024-07-08
 // MIT 14
