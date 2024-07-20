5d1ee88769119303121881d2398dc3fb33efebe6
aris1377 <bek77519101@gmail.com>
2024-07-20 20:17:56 +0900

    feat: develop getTopUsers rest Api

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -82,6 +82,19 @@ class MemberService {
     return result as Member;
   }
 
+  public async getTopUsers(): Promise<Member[]> {
+    const result = await this.memberModel
+      .find({
+        memberStatus: MemberStatus.ACTIVE,
+        memberPoints: { $gte: 1 },
+      })
+      .sort({ memberPoints: -1 })
+      .limit(4)
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result as Member[];
+  }
+
   /** SSR */
   public async processSignup(input: MemberInput): Promise<Member> {
     //faqat bitta restaran kiritilish uchun mantiq yozamiz
